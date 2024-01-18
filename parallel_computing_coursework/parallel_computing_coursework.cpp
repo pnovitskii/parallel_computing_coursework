f57ffb39650f23758e468e917ed02a2278723cad
Pavel Novytskyi <pavlo.novytskyi@chisw.com>
2024-01-18 12:50:43 +0200

    create server

--- parallel_computing_coursework/parallel_computing_coursework.cpp
+++ parallel_computing_coursework/parallel_computing_coursework.cpp
@@ -1,7 +1,25 @@
 ﻿#include <iomanip>
 #include "inverted_index.h"
+//#include "server.h"
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
+using std::cout;
+using std::endl;
 
 inline constexpr auto path = "datasets";
+
+std::string read_(tcp::socket& socket) {
+    boost::asio::streambuf buf;
+    boost::asio::read_until(socket, buf, "\n");
+    std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+    return data;
+}
+void send_(tcp::socket& socket, const std::string& message) {
+    const std::string msg = message + "\n";
+    boost::asio::write(socket, boost::asio::buffer(message));
+}
 
 void testCase(int numThreads) {
     std::cout << "THREADS: " << std::setw(3) << numThreads << " ";// << endl << endl;
@@ -17,11 +35,6 @@ void testCase(int numThreads) {
 void test() {
     for (int i = 1; i <= 16; i++)
         testCase(i);
-}
-
-int main()
-{
-    srand(time(NULL));
     test();
     indexing::InvertedIndex index;
     index.index(path, 4);
@@ -31,10 +44,46 @@ int main()
     auto result = index.find(word);
     if (result.empty()) {
         std::cout << "No such entry!\n";
-        return 0;
+        return;
     }
     auto last = std::unique(result.begin(), result.end());
     std::size_t uniqueCount = std::distance(result.begin(), last);
     std::cout << "Word: " << word << std::endl;
     std::cout << "Found " << result.size() << " entries in " << uniqueCount << " file" << (uniqueCount > 1 ? "s" : "") << ".\n";
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    boost::asio::io_service io_service;
+    //listen for new connection
+    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234));
+    //socket creation 
+    tcp::socket socket_(io_service);
+    //waiting for connection
+    acceptor_.accept(socket_);
+
+    while (1) {
+        //read operation
+        std::string message = read_(socket_);
+        cout << message << endl;
+        //write operation
+        //send_(socket_, "Hello From Server!");
+        //cout << "Servent sent Hello message to Client!" << endl;
+    }
+
+
+    //try {
+    //    boost::asio::io_service io_service;
+
+    //    // Start server on port 12345
+    //    TCPServer server(io_service, 12345);
+
+    //    // Run the IO service
+    //    io_service.run();
+    //}
+    //catch (std::exception& e) {
+    //    std::cerr << "Exception: " << e.what() << std::endl;
+    //}
 }
